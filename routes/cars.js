f4828f6e066cc529baafd72c3c84f93b1ff22ab5
Hery Tahina Rakotonaivo <heryrak.hr@gmail.com>
2023-01-30 17:17:16 +0300

    feature validate exit request

--- routes/cars.js
+++ routes/cars.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { depositCar, getCars, getCarRepairs, getRepairsHistory, createExitRequest, getNonReceivedCars, addRepairs, listReceivedCars, updateRepairProgress } = require('../services/car');
+const { depositCar, getCars, getCarRepairs, getRepairsHistory, createExitRequest, getNonReceivedCars, addRepairs, listReceivedCars, updateRepairProgress, exitRequestList, exitTicketValidation } = require('../services/car');
 const { addClientCar, carReception } = require('../services/user');
 
 router.get('/:immatriculation/repairs/history', getRepairsHistory);
@@ -10,6 +10,8 @@ router.route('/:immatriculation/repairs')
 .post(addRepairs)
 .put(updateRepairProgress);
 
+router.put('/:id/recover/validate', exitTicketValidation);
+
 router.put('/:immatriculation/recover', createExitRequest);
 
 router.put('/:immatriculation/reception', carReception);
@@ -17,6 +19,8 @@ router.put('/:immatriculation/reception', carReception);
 router.route('/')
 .get(getCars)
 .post(addClientCar);
+
+router.get('/requested', exitRequestList);
 
 router.get('/received', listReceivedCars);
 
